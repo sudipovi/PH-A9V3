7a995c5f28bf2c7a52d782e503b58ee1e10a625a
sudipovi <shudipdey73@gmail.com>
2026-02-01 01:25:54 +0600

    image uploaded

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../component/Navbar";
 import Slider from "../component/Slider";
+import Popular from "../component/Popular";
 
 const Home = () => {
   return (
     <div>
       <Slider></Slider>
+      <Popular></Popular>
     </div>
   );
 };
---
+++ src/component/Popular.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Popular = () => {
+  return (
+    <div>
+      <div>
+        <h3>Popular Winter Care Services</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
